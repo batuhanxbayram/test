a0703e8ca9c81c032053b4f6d00b1c3a480d3e7e
batuhanxbayram <batuhanbayramofficial@hotmail.com>
2025-09-22 18:05:29 +0300

    backend-sign-in-page

--- src/routes.jsx
+++ src/routes.jsx
@@ -9,6 +9,7 @@ import {
 import { Home, Profile, Tables, Notifications } from "@/pages/dashboard";
 import { SignIn } from "@/pages/auth";
 
+
 const icon = {
   className: "w-5 h-5 text-inherit",
 };
@@ -22,24 +23,29 @@ export const routes = [
         name: "dashboard",
         path: "/home",
         element: <Home />,
+        // roles eklenmediği için giriş yapan herkes görebilir.
+        // İsterseniz roles: ['admin', 'user'] de ekleyebilirsiniz.
       },
       {
         icon: <UserCircleIcon {...icon} />,
-        name: "profile",
+        name: "AşktanemDilara",
         path: "/profile",
         element: <Profile />,
+        // roles eklenmediği için giriş yapan herkes görebilir.
       },
       {
         icon: <TableCellsIcon {...icon} />,
         name: "tables",
         path: "/tables",
         element: <Tables />,
+        roles: ['admin'], // YENİ: Sadece adminler görebilir.
       },
       {
         icon: <InformationCircleIcon {...icon} />,
         name: "notifications",
         path: "/notifications",
         element: <Notifications />,
+        roles: ['admin'], // YENİ: Sadece adminler görebilir.
       },
     ],
   },
